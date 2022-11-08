f01edbfd7bcdfdd6e564aa696e67f6c313805bf1
muhammadalief19 <aliefmuhammad1908@gmail.com>
2022-11-09 05:54:06 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <div className="w-full">
+        <Navbar />
+        <div className="container mx-auto"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+export default function Navbar() {
+  const [hamburgerActive, setHamburgerActive] = useState(false);
+
+  function hamburgerMenu() {
+    setHamburgerActive((hamburgerActive) => !hamburgerActive);
+  }
+  return (
+    <>
+      <nav className="w-full px-7 py-5 bg-red-600 text-white grid grid-cols-3 h-max sticky top-0">
+        <div className="w-full font-bold">
+          <p className="text-3xl">Pahlawan</p>
+        </div>
+        <div className="col-span-2 w-full flex justify-end h-full items-center">
+          <div className={`${hamburgerActive ? 'flex' : 'hidden'} lg:flex absolute top-20 right-3 bg-slate-400 lg:bg-transparent lg:static w-28 lg:w-1/2 rounded`}>
+            <ul className="w-full h-44 lg:h-max flex lg:flex-row flex-col justify-around items-center">
+              <li className="">
+                <a href="" className="">
+                  Dashboard
+                </a>
+              </li>
+              <li className="">
+                <a href="" className="">
+                  About
+                </a>
+              </li>
+              <li className="">
+                <a href="" className="">
+                  Partner
+                </a>
+              </li>
+              <li className="">
+                <a href="" className="">
+                  Member
+                </a>
+              </li>
+              <li className="">
+                <a href="" className="">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div onClick={hamburgerMenu} className={`lg:hidden flex flex-col w-7 h-6 justify-between ${hamburgerActive ? 'hamburger-active' : ''} cursor-pointer`}>
+            <span className="w-full h-1 bg-white rounded-full transition duration-300 ease-in-out origin-top-left"></span>
+            <span className="w-full h-1 bg-white rounded-full transition duration-300 ease-in-out"></span>
+            <span className="w-full h-1 bg-white rounded-full transition duration-300 ease-in-out origin-bottom-left"></span>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
