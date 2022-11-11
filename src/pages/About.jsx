cfbf7047d2865d96048a2cef16fe2e2b397c5cde
vrayoga <vrayogal227@gmail.com>
2022-11-11 20:26:33 +0700

    merevisi tampilan about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,35 +6,35 @@ function About() {
       <div className="w-full relative px-7 py-10">
         <p className="text-4xl font-bold text-center mb-5">About</p>
         <p className="w-3/4 text-justify mx-auto text-gray-500">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-          similique, cupiditate laudantium sapiente quas, omnis voluptatem autem
-          at rem nobis modi tenetur ipsam accusantium. Laboriosam omnis
-          reiciendis excepturi, maiores tenetur quae beatae ipsam laudantium
-          pariatur illum! Error quod ex quidem quia dolorum quo nostrum
-          similique magnam, pariatur veniam aliquid, totam consectetur suscipit
-          corrupti. Inventore dicta reprehenderit ullam ipsum, unde, aliquam
-          dolore vel repudiandae dolores corporis, quas earum. Quae architecto,
-          cupiditate recusandae placeat quibusdam enim rerum voluptates
-          voluptatum, ab ea tenetur dicta minus dolore possimus? Error placeat,
-          cupiditate a voluptas recusandae, aliquam nesciunt distinctio est,
-          deleniti doloribus similique impedit ab praesentium. Excepturi beatae
-          et iusto dicta neque unde! Repellendus dolores veniam voluptatibus
-          ullam eaque aspernatur numquam eius repellat atque ipsam? Nobis
-          repudiandae odio fuga atque quasi explicabo optio enim earum possimus.
-          In adipisci tempore, dolore iure quaerat fugit minus sed aut maxime
-          voluptatum optio, velit rerum mollitia voluptas quam odio, temporibus
-          aliquam ab a. Maxime beatae neque quo dolor nobis! Ipsum eum iure
-          quod, vitae molestiae quam recusandae accusamus omnis vero. Possimus
-          voluptates expedita voluptas. Repellendus totam vel excepturi sequi
-          commodi consequatur nobis nihil architecto facilis voluptates natus
-          quasi, qui culpa similique molestiae corrupti voluptas beatae eos
-          officia, perspiciatis itaque accusamus ratione! Delectus ratione
-          dolorum totam ipsam voluptates magnam. Architecto rem voluptate odit
-          natus possimus non unde earum obcaecati et necessitatibus iste quam
-          quisquam aut laudantium commodi ea quia ullam impedit similique,
-          dolorum, hic perspiciatis sit maiores eligendi. Suscipit laborum
-          labore natus, odio laboriosam dolorum repellendus animi mollitia
-          perferendis, dolorem tenetur?
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae
+          voluptatibus facilis reprehenderit, dolorum architecto voluptates
+          aspernatur mollitia quos id magni delectus ea necessitatibus dolores
+          iure neque officia sed aperiam vel soluta! Sit eligendi minima
+          voluptatibus, animi magnam numquam blanditiis nostrum, eius, est vitae
+          ex molestias voluptatum. Tenetur suscipit quidem esse itaque
+          consectetur quas pariatur animi, sit deserunt! Perspiciatis at
+          temporibus nesciunt accusantium eum est ullam, saepe quis neque modi
+          distinctio cupiditate hic! Placeat dolore quos assumenda minima
+          quaerat, veniam aspernatur facilis praesentium dolorem ea voluptatum
+          maiores accusantium, excepturi inventore ullam, a saepe iusto
+          voluptatem asperiores dignissimos nam velit consectetur ipsam fuga!
+          Quasi, quaerat dolorum debitis animi qui earum. Cupiditate
+          reprehenderit repellendus aperiam quos sed, ex temporibus tempore
+          perferendis rem, alias tenetur praesentium ut odit corporis, maxime
+          architecto magni? Fuga suscipit a, repudiandae veritatis illo nostrum
+          eligendi alias id dolorum quae earum autem laboriosam ipsa repellat
+          tempora saepe optio, tenetur explicabo aliquam dicta. Tempora, et.
+          Delectus consectetur libero tenetur? Earum eos deserunt reprehenderit
+          ad voluptatum omnis minus nemo ullam cum numquam, exercitationem esse
+          sint aperiam suscipit adipisci voluptate! Earum sit voluptatum dolorem
+          dolor provident vel libero a rerum eaque, voluptas quidem nobis ad
+          voluptate, nam similique, ipsa ipsum voluptates quasi. Ipsum facilis
+          perferendis temporibus necessitatibus, nisi doloribus? Dolores nihil
+          quasi, omnis nam tenetur sint doloribus error eos! Dolorem repellendus
+          saepe suscipit vero sequi earum voluptates, autem adipisci, non
+          accusamus perferendis excepturi fugit doloribus nisi, maiores corrupti
+          dicta corporis magnam ipsum quidem! Harum officiis sunt modi nihil
+          facilis excepturi, in ut ad?
         </p>
         <img
           src={asset1}
