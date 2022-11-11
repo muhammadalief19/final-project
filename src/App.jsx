2bcefc5a631555f5a214fb8a68a94cf741906e44
muhammadalief19 <aliefmuhammad1908@gmail.com>
2022-11-11 19:56:58 +0700

    merevisi tampilan v3

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
-import Dashboard from './pages/Dashboard';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
 
 function App() {
   return (
     <>
       <div className="w-full">
-        <Navbar name={'Iyek Company'} />
+        <Navbar name={"Iyek Company"} />
         <div className="w-full">
           <Dashboard />
+          <About />
         </div>
       </div>
     </>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import bg_db from '../assets/bg-dashboard.jpg';
-import img1 from '../assets/company.svg';
+import React from "react";
+import bg_db from "../assets/bg-dashboard.jpg";
+import img1 from "../assets/company.svg";
 function Dashboard() {
   return (
     <>
-      <div className="w-full px-7 py-20 bg-teal-600 text-white box-border grid grid-cols-2 gap-x-14 h-max" id="dashboard">
-        <div className="w-full flex h-full justify-center items-center">
-          <div className="w-max">
-            <p className="text-6xl font-extrabold w-40 mb-5">IYEK COMPANY</p>
-            <p className="w-64 font-normal text-lg">Kami melayani dengan sepenuh hati dan senyuman</p>
+      <div className="w-full  bg-teal-600" id="dashboard">
+        <div className="w-full px-7 py-20 text-white box-border grid grid-cols-2 gap-x-14 h-max">
+          <div className="w-full flex h-full justify-center items-center">
+            <div className="w-max">
+              <p className="text-6xl font-extrabold w-40 mb-5">IYEK COMPANY</p>
+              <p className="w-64 font-normal text-lg">
+                Kami melayani dengan sepenuh hati dan senyuman
+              </p>
+            </div>
+          </div>
+          <div className="w-full flex justify-center">
+            <img src={img1} alt="" className="w-3/4" />
           </div>
         </div>
-        <div className="w-full flex justify-center">
-          <img src={img1} alt="" className="w-3/4" />
-        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#fff"
+            fill-opacity="1"
+            d="M0,96L60,90.7C120,85,240,75,360,96C480,117,600,171,720,197.3C840,224,960,224,1080,208C1200,192,1320,160,1380,144L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+          ></path>
+        </svg>
       </div>
     </>
   );
