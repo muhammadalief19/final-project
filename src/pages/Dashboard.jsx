5fcd78b35904a05d23a4a397bb558a809f0f2be5
muhammadalief19 <aliefmuhammad1908@gmail.com>
2022-11-12 10:59:17 +0700

    mengatur responsve section dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,17 +1,15 @@
-import React from "react";
-import bg_db from "../assets/bg-dashboard.jpg";
-import img1 from "../assets/company.svg";
+import React from 'react';
+import bg_db from '../assets/bg-dashboard.jpg';
+import img1 from '../assets/company.svg';
 function Dashboard() {
   return (
     <>
-      <div className="w-full  bg-teal-600" id="dashboard">
-        <div className="w-full px-7 py-20 text-white box-border grid grid-cols-2 gap-x-14 h-max">
+      <div className="w-full bg-teal-600" id="dashboard">
+        <div className="w-full px-7 py-28 lg:py-20 text-white box-border grid grid-flow-row lg:grid-cols-2 lg:gap-x-14 gap-y-16 h-max">
           <div className="w-full flex h-full justify-center items-center">
             <div className="w-max">
               <p className="text-6xl font-extrabold w-40 mb-5">IYEK COMPANY</p>
-              <p className="w-64 font-normal text-lg">
-                Kami melayani dengan sepenuh hati dan senyuman
-              </p>
+              <p className="w-64 font-normal text-lg">Kami melayani dengan sepenuh hati dan senyuman</p>
             </div>
           </div>
           <div className="w-full flex justify-center">
