e4afb4bf61b75c682c33cf6f395783465954ecef
muhammadalief19 <aliefmuhammad1908@gmail.com>
2022-11-16 20:54:14 +0700

    merevisi UI member section

--- src/pages/Member.jsx
+++ src/pages/Member.jsx
@@ -1,22 +1,49 @@
 import React from "react";
-import foto1 from "../assets/foto1.svg";
-import foto2 from "../assets/foto-2.svg";
+import foto1 from "../assets/nabil.webp";
+import foto2 from "../assets/yoga.webp";
+import foto3 from "../assets/alief.webp";
 
 function Member() {
     return(
         <>
-        <div className="w-full flex justify-center py-20" id="member">
-            <div className="w-3/4 grid grid-cols-3">
-                <div className="">
-                    <img src="" alt="" className="" />
+        <div className="w-full flex flex-col items-center py-20" id="member">
+            <p className="text-center lg:mb-16 font-bold text-4xl text-teal-600">Members</p>
+            <div className="w-3/4 grid grid-rows-3 gap-y-10 ">
+                <div className="w-full flex justify-center h-max items-center hover:scale-110 duration-500 transition-all ease-out cursor-pointer">
+                    <div className="w-[60%] box-border flex rounded-md overflow-hidden border border-slate-300">
+                    <div className="w-max">
+                    <img src={foto3} alt="" className="w-64" />
+                    </div>
+                    <div className="w-full flex flex-col justify-center px-3 py-2">
+                        <p className="text-xl font-bold mb-2">Muhammad Alief Putra Pratama</p>
+                        <p className="font-semibold mb-2">3122522023</p>
+                        <p className="">Fullstack Developer</p>
+                    </div>
+                    </div>
                 </div>
-                <div className="w-full flex flex-col justify-center h-max items-center">
-                    <p className="text-xl font-bold">Nabil</p>
-                    <img src={foto1} alt="" className="lg:w-1/2 w-3/4" />
+                <div className="w-full flex justify-center h-max items-center hover:scale-110 duration-500 transition-all ease-out cursor-pointer">
+                    <div className="w-[60%] box-border flex rounded-md overflow-hidden border border-slate-300">
+                    <div className="w-max">
+                    <img src={foto1} alt="" className="w-64" />
+                    </div>
+                    <div className="w-full flex flex-col justify-center px-3 py-2">
+                        <p className="text-xl font-bold mb-2">Muhammad Nabil Royyan</p>
+                        <p className="font-semibold mb-2">3122522027</p>
+                        <p className="">Cyber Security</p>
+                    </div>
+                    </div>
                 </div>
-                <div className="w-full flex flex-col justify-center h-max items-center">
-                    <p className="text-xl font-bold">Yoga</p>
-                    <img src={foto2} alt="" className="lg:w-1/2 w-3/4" />
+                <div className="w-full flex justify-center h-max items-center hover:scale-110 duration-500 transition-all ease-out cursor-pointer">
+                    <div className="w-[60%] box-border flex rounded-md overflow-hidden border border-slate-300">
+                    <div className="w-max">
+                    <img src={foto2} alt="" className="w-64" />
+                    </div>
+                    <div className="w-full flex flex-col justify-center px-3 py-2">
+                        <p className="text-xl font-bold mb-2">Varyoga Loreansa</p>
+                        <p className="font-semibold mb-2">3122522005</p>
+                        <p className="">Cyber Security</p>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
