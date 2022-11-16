4739549ee3bed962acc38bf717a271aee7edf7ba
muhammadalief19 <aliefmuhammad1908@gmail.com>
2022-11-16 14:38:30 +0700

    menambahkan id partner

--- src/pages/Partner.jsx
+++ src/pages/Partner.jsx
@@ -6,7 +6,7 @@ import partner4 from "../assets/asset-7.svg"
 function Partner() {
 return (
     <>
-    <div className="w-full bg-teal-600 text-white">
+    <div className="w-full bg-teal-600 text-white" id="partner">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,160L60,176C120,192,240,224,360,208C480,192,600,128,720,133.3C840,139,960,213,1080,224C1200,235,1320,181,1380,154.7L1440,128L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
     <p className="text-4xl font-bold text-center mb-16">Partner</p>
         <div className="w-[90%] grid grid-cols-4 mx-auto items-center gap-24 py-10">
