05d51affaf2a5fbb8a3d455d342b797e1dfd25b0
muhammadalief19 <aliefmuhammad1908@gmail.com>
2022-11-17 07:31:50 +0700

    mengubah warna background section Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Contact() {
     return(
         <>
-        <div className="w-full">
+        <div className="w-full bg-teal-600">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,256L48,261.3C96,267,192,277,288,266.7C384,256,480,224,576,197.3C672,171,768,149,864,165.3C960,181,1056,235,1152,234.7C1248,235,1344,181,1392,154.7L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
         </div>
         </>
