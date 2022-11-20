cd68af1ced9f6ef5d960b663c498a5b7da460b44
muhammadalief19 <aliefmuhammad1908@gmail.com>
2022-11-20 16:53:41 +0700

    merevisi UI footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
   return (
     <>
       <div className="w-full flex justify-center py-7">
-        <div className="w-3/4 grid grid-flow-row gap-y-5">
+        <div className="w-[90%] md:w-3/4 grid grid-flow-row gap-y-5">
           <div className="w-full">
             <p className="text-xl  md:text-2xl lg:text-3xl font-bold text-teal-600">
               Terima Kasih Kawan
@@ -21,20 +21,32 @@ function Footer() {
             </p>
             <div className="mx-auto md:m-0 w-40 flex justify-between py-5">
               <a href="" className="">
-                <FontAwesomeIcon class="w-5 text-teal-600" icon={faInstagram} />
+                <FontAwesomeIcon
+                  class="w-5 text-teal-600 hover:text-teal-800 transition-colors duration-500 ease-in-out"
+                  icon={faInstagram}
+                />
               </a>
               <a href="" className="">
-                <FontAwesomeIcon class="w-5 text-teal-600" icon={faGithub} />
+                <FontAwesomeIcon
+                  class="w-5 text-teal-600 hover:text-teal-800 transition-colors duration-500 ease-in-out"
+                  icon={faGithub}
+                />
               </a>
               <a href="" className="">
-                <FontAwesomeIcon class="w-5 text-teal-600" icon={faTelegram} />
+                <FontAwesomeIcon
+                  class="w-5 text-teal-600 hover:text-teal-800 transition-colors duration-500 ease-in-out"
+                  icon={faTelegram}
+                />
               </a>
               <a href="" className="">
-                <FontAwesomeIcon class="w-5 text-teal-600" icon={faTwitter} />
+                <FontAwesomeIcon
+                  class="w-5 text-teal-600 hover:text-teal-800 transition-colors duration-500 ease-in-out"
+                  icon={faTwitter}
+                />
               </a>
             </div>
           </div>
-          <div className="w-full">
+          <div className="w-full border-t-2 border-t-gray-400 pt-5">
             <p className="text-center text-sm lg:text-base font-semibold text-gray-400">
               Copyright © 2022 Iyek Company
             </p>
