4f0eda082785ff12d27d88947ff0e847cf83c479
muhammadalief19 <aliefmuhammad1908@gmail.com>
2022-11-20 14:45:37 +0700

    mempercantik UI footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,44 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import {
+  faGithub,
+  faInstagram,
+  faTelegram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
     <>
-      <div className="w-full bg-white px-5 py-3 grid grid-cols-5">
-        <div className="w-full">
-          <p className="w-1/4 font-bold">Iyek Company</p>
+      <div className="w-full flex justify-center py-7">
+        <div className="w-3/4 grid grid-flow-row gap-y-5">
+          <div className="w-full">
+            <p className="text-3xl font-bold text-teal-600">
+              Terima Kasih Kawan
+            </p>
+            <p className="font-medium text-lg">
+              Terima kasih, telah bekunjung di website kami
+            </p>
+            <div className="w-40 flex justify-between">
+              <a href="" className="">
+                <FontAwesomeIcon class="w-5" icon={faInstagram} />
+              </a>
+              <a href="" className="">
+                <FontAwesomeIcon class="w-5" icon={faGithub} />
+              </a>
+              <a href="" className="">
+                <FontAwesomeIcon class="w-5" icon={faTelegram} />
+              </a>
+              <a href="" className="">
+                <FontAwesomeIcon class="w-5" icon={faTwitter} />
+              </a>
+            </div>
+          </div>
+          <div className="w-full">
+            <p className="text-center font-semibold text-gray-400">
+              Copyright © 2022 Iyek Company
+            </p>
+          </div>
         </div>
       </div>
     </>
